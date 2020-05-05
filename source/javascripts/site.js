8a2248ff749659117ac56e6aae435ded73cfee81
ml7757 <matthew.laporte@tutanota.com>
2020-05-05 14:15:58 +0200

    create location section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
-<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+// <script>
+// 	function initMap() {
+// 	  var montys = {lat: 49.169, lng: -121.941};
+// 	  var map = new google.maps.Map(
+// 	      document.getElementById('map'), {zoom: 4, center: montys});
+// 	  var marker = new google.maps.Marker({position: montys, map: map});
+// 	}
+// 	</script>
+// 	<script async defer
+// 	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCzZ-yG3tL0ylOocv_d7xShBSxvKMxSlew&callback=initMap">
+// </script>
+// <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
