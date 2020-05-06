3bf69a55c0df6eef6d3fb3aa595642bca78da99d
ml7757 <matthew.laporte@tutanota.com>
2020-05-06 17:49:44 +0200

    add ruby -v and activate minify for build

--- config.rb
+++ config.rb
@@ -44,7 +44,7 @@ activate :sprockets
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
